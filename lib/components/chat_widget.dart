bcc46d15f1f9d7ffd61af5cfa9f10ac7f293d73f
safwanjmuhammed <safwanjmuhammed@gmail.com>
2024-01-31 18:23:25 +0530

    chat_widget_created

---
+++ lib/components/chat_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+Widget chat(BuildContext context, String geminiText, String chatControllerText,
+    Brightness platformBrightness) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: Color(0XFF50577A),
+              child: Text(
+                'SA',
+                style: platformBrightness == Brightness.light
+                    ? const TextStyle(color: Colors.white)
+                    : Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Text('YOU', style: Theme.of(context).textTheme.bodyMedium),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 50),
+          child: Text(
+            chatControllerText,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        //// GEMINI CHAT
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: const Color(0XFF50577A),
+                child: Text(
+                  'G',
+                  style: platformBrightness == Brightness.light
+                      ? const TextStyle(color: Colors.white)
+                      : Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text('GEMINI', style: Theme.of(context).textTheme.bodyMedium),
+            ],
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.only(left: 50),
+            child: geminiText.isEmpty
+                ? Text(
+                    'Generating.....',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  )
+                : Text(
+                    geminiText,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  )),
+      ],
+    ),
+  );
+}
--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gemini/components/chat_widget.dart';
 import 'package:gemini/services/gemini_service.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -69,7 +70,12 @@ class _ChatScreenState extends State<ChatScreen> {
             isGenerating
                 ? Expanded(
                     child: ListView(children: [
-                    chat(context, chatText, platformBrightness)
+                    chat(
+                      context,
+                      geminiText,
+                      chatText,
+                      platformBrightness,
+                    )
                   ]))
                 : const SizedBox(),
             Padding(
@@ -120,70 +126,6 @@ class _ChatScreenState extends State<ChatScreen> {
                 ))
           ],
         ),
-      ),
-    );
-  }
-
-  Widget chat(BuildContext context, String chatControllerText,
-      Brightness platformBrightness) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                backgroundColor: Color(0XFF50577A),
-                child: Text(
-                  'SA',
-                  style: platformBrightness == Brightness.light
-                      ? const TextStyle(color: Colors.white)
-                      : Theme.of(context).textTheme.bodyMedium,
-                ),
-              ),
-              const SizedBox(width: 10),
-              Text('YOU', style: Theme.of(context).textTheme.bodyMedium),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 50),
-            child: Text(
-              chatControllerText,
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-          ),
-          //// GEMINI CHAT
-          Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: const Color(0XFF50577A),
-                  child: Text(
-                    'G',
-                    style: platformBrightness == Brightness.light
-                        ? const TextStyle(color: Colors.white)
-                        : Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Text('GEMINI', style: Theme.of(context).textTheme.bodyMedium),
-              ],
-            ),
-          ),
-          Padding(
-              padding: const EdgeInsets.only(left: 50),
-              child: geminiText.isEmpty
-                  ? Text(
-                      'Generating.....',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    )
-                  : Text(
-                      geminiText,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    )),
-        ],
       ),
     );
   }
